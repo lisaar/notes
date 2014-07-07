eb698c2fa190e92810b545c0bae8d1635af630be
Lisa A-R <lisaar@gmail.com>
2014-07-07 10:10:12 +0100

    first commit

---
+++ rspec.rb
@@ -0,0 +1,21 @@
+Cucumber
+	Feature narrative
+	Scenario
+		Given, when, then principle to document unexplored scenarios
+
+
+RSpec
+	domain spicific language for specifying the behaviour of objects
+	uses expectations: an expression of how the subject code is expected to behave
+		state-based expectations
+		interactive expectations
+	Example group
+		group of code examples (test cases) that comprise a feature
+	Code examples
+		example of how subject code can be used
+		how subject code will behave in a specific context
+		become regression tests once code is written
+
+Code examples
+	describe() #method to define an example group
+
